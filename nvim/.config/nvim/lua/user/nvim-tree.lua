4c2a74fb72bc7fcf52dcdded26b75569e10c59f8
Micheal Zinc <me@michealzinc.com>
2022-05-15 11:27:49 +0800

    Neovim basic setup completed

---
+++ nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -0,0 +1,170 @@
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+  },
+  folder = {
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+  },
+}
+
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  vim.notify("nvim-tree not found!")
+  return
+end
+
+nvim_tree.setup({
+  auto_reload_on_write = true,
+  disable_netrw = false,
+  hijack_cursor = false,
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = false,
+  ignore_buffer_on_setup = false,
+  open_on_setup = false,
+  open_on_setup_file = false,
+  open_on_tab = false,
+  sort_by = "name",
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = "left",
+    preserve_window_proportions = false,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+    mappings = {
+      custom_only = false,
+      list = {
+        -- user mappings go here
+        { key = { "l", "<CR>", "o" }, action = "edit" },
+        { key = "h", action = "close_node" },
+        { key = "v", action = "vsplit" },
+        { key = "O", action = "cd" },
+      },
+    },
+  },
+  renderer = {
+    indent_markers = {
+      enable = false,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+    },
+  },
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
+  ignore_ft_on_setup = {},
+  system_open = {
+    cmd = "",
+    args = {},
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  filters = {
+    dotfiles = false,
+    custom = {},
+    exclude = {},
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 400,
+  },
+  actions = {
+    use_system_clipboard = true,
+    change_dir = {
+      enable = true,
+      global = false,
+      restrict_above_cwd = false,
+    },
+    open_file = {
+      quit_on_open = false,
+      resize_window = true,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      diagnostics = false,
+      git = false,
+      profile = false,
+    },
+  },
+}
+)
+
+--
+-- with relative path
+require "nvim-tree.events".on_file_created(function(file) vim.cmd("edit " .. file.fname) end)
+-- 默认按键
+-- o     ：打开目录或文件
+-- a     ：新增目录或文件
+-- r     ：重命名目录或文件
+-- x     ：剪切目录或文件
+-- c     ：复制目录或文件
+-- d     ：删除目录或文件
+-- y     ：复制目录或文件名称
+-- Y     ：复制目录或文件相对路径
+-- gy    ：复制目录或文件绝对路径
+-- p     ：粘贴目录或文件
+-- s     ：使用系统默认程序打开目录或文件
+-- <Tab> ：将文件添加到缓冲区，但不移动光标
+-- <C-v> ：垂直分屏打开文件
+-- <C-x> ：水平分屏打开文件
+-- <C-]> ：进入光标下的目录
+-- <C-r> ：重命名目录或文件，删除已有目录名称
+-- -     ：返回上层目录
+-- I     ：切换隐藏文件/目录的可见性
+-- H     ：切换点文件的可见性
+-- R     ：刷新资源管理器
+-- 另外，文件资源管理器操作和操作文档方式一致，可按 / ? 进行搜索
+
